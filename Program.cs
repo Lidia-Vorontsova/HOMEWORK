340c72cacdd36bf383eed6eb34d147e5b84b9771
Lidia <lidiya_vorontsov@mail.ru>
2022-07-11 16:06:23 +0300

    task2

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB){
+    Console.WriteLine("numberA наибольшее число,чем numberB");
+}
+else
+{
+    Console.WriteLine("numberB наибольшее число,чем numberA");
+}
+Console.WriteLine();
